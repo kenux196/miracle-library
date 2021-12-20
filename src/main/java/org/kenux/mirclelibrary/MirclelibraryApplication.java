c3a872bcbbb3b76dbf727c6cb7ccbd2256f84653
kenux196 <kenux.yun@gmail.com>
2021-12-20 20:11:52 +0900

    init project

---
+++ src/main/java/org/kenux/mirclelibrary/MirclelibraryApplication.java
@@ -0,0 +1,13 @@
+package org.kenux.mirclelibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MirclelibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MirclelibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/kenux/mirclelibrary/MirclelibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kenux.mirclelibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MirclelibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
