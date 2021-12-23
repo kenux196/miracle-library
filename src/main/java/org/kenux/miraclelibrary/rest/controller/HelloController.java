8f9f2beedc01244f548cde108a3fe1dbc1531d80
kenux196 <kenux.yun@gmail.com>
2021-12-23 17:58:48 +0900

    Remove not used parameter

--- src/main/java/org/kenux/miraclelibrary/rest/controller/HelloController.java
+++ src/main/java/org/kenux/miraclelibrary/rest/controller/HelloController.java
@@ -1,6 +1,5 @@
 package org.kenux.miraclelibrary.rest.controller;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping
-    String hello(Model model) {
+    String hello() {
         return "hello";
     }
 }
