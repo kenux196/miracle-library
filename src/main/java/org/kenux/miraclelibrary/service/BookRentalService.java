3e2882ba6d0700324ea3d7bd2bfeb7d212d75ab0
kenux196 <kenux.yun@gmail.com>
2021-12-25 23:45:23 +0900

    Add BookRentalService class and rental book test

--- src/main/java/org/kenux/miraclelibrary/service/BookRentalService.java
+++ src/main/java/org/kenux/miraclelibrary/service/BookRentalService.java
@@ -1,4 +1,35 @@
 package org.kenux.miraclelibrary.service;
 
+import lombok.RequiredArgsConstructor;
+import org.kenux.miraclelibrary.domain.Book;
+import org.kenux.miraclelibrary.domain.BookRental;
+import org.kenux.miraclelibrary.domain.Member;
+import org.kenux.miraclelibrary.repository.BookRentalRepository;
+import org.kenux.miraclelibrary.repository.BookRepository;
+import org.kenux.miraclelibrary.repository.MemberRepository;
+import org.kenux.miraclelibrary.rest.dto.RequestBookRental;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class BookRentalService {
+
+    private final MemberRepository memberRepository;
+    private final BookRepository bookRepository;
+    private final BookRentalRepository bookRentalRepository;
+
+    public BookRental rentalBook(RequestBookRental requestBookRental) {
+        Optional<Member> member = memberRepository.findById(requestBookRental.getMemberId());
+        Optional<Book> book = bookRepository.findById(requestBookRental.getBookId());
+
+        BookRental bookRental = BookRental.builder()
+                .member(member.get())
+                .book(book.get())
+                .rentalStartDate(LocalDate.now())
+                .build();
+        return bookRentalRepository.save(bookRental);
+    }
 }
--- src/test/java/org/kenux/miraclelibrary/service/BookRentalServiceTest.java
+++ src/test/java/org/kenux/miraclelibrary/service/BookRentalServiceTest.java
@@ -3,22 +3,38 @@ package org.kenux.miraclelibrary.service;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.kenux.miraclelibrary.domain.Book;
+import org.kenux.miraclelibrary.domain.BookRental;
 import org.kenux.miraclelibrary.domain.Member;
+import org.kenux.miraclelibrary.domain.enums.MemberRole;
+import org.kenux.miraclelibrary.repository.BookRentalRepository;
 import org.kenux.miraclelibrary.repository.BookRepository;
+import org.kenux.miraclelibrary.repository.MemberRepository;
 import org.kenux.miraclelibrary.rest.dto.RequestBookRental;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.kenux.miraclelibrary.domain.QBook.book;
-import static org.kenux.miraclelibrary.domain.QMember.member;
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class BookRentalServiceTest {
 
     @Mock
+    MemberRepository memberRepository;
+
+    @Mock
     BookRepository bookRepository;
+
+    @Mock
+    BookRentalRepository bookRentalRepository;
 
     @InjectMocks
     BookRentalService bookRentalService;
@@ -27,16 +43,37 @@ class BookRentalServiceTest {
     @DisplayName("멤버는 책을 대여한다.")
     void test_BookRentalService_rentalBook() throws Exception {
         // given
+        BookRental bookRental = BookRental.builder()
+                .member(getMember())
+                .book(getBook1())
+                .build();
+
+        when(memberRepository.findById(any())).thenReturn(Optional.of(getMember()));
+        when(bookRepository.findById(any())).thenReturn(Optional.of(getBook1()));
+        when(bookRentalRepository.save(any())).thenReturn(bookRental);
+
+        // when
         Long memberId = 1L;
         Long bookId = 1L;
         RequestBookRental requestBookRental = new RequestBookRental(memberId, bookId);
-
-
-        // when
-//        bookRentalService.rentalBook(requestBookRental);
+        BookRental saved = bookRentalService.rentalBook(requestBookRental);
 
         // then
+        assertThat(saved.getBook().getId()).isEqualTo(bookId);
+        assertThat(saved.getMember().getId()).isEqualTo(memberId);
 
+    }
+
+    private Member getMember() {
+        Member member = new Member("member1", "member1@test.com", "password", MemberRole.CUSTOMER);
+        ReflectionTestUtils.setField(member, "id", 1L);
+        return member;
+    }
+
+    private Book getBook1() {
+        Book book = new Book("title", "author", "isbn", LocalDate.of(2020, 12, 20));
+        ReflectionTestUtils.setField(book, "id", 1L);
+        return book;
     }
 
 }
