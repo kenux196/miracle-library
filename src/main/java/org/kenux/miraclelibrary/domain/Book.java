d192f5a79fbb28d0830d9cb919d34eb354166843
kenux196 <kenux.yun@gmail.com>
2021-12-28 16:11:02 +0900

    Modify Book Entity

--- src/main/java/org/kenux/miraclelibrary/domain/Book.java
+++ src/main/java/org/kenux/miraclelibrary/domain/Book.java
@@ -49,7 +49,6 @@ public class Book {
         this.status = status;
     }
 
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
--- src/test/java/org/kenux/miraclelibrary/domain/BookTest.java
+++ src/test/java/org/kenux/miraclelibrary/domain/BookTest.java
@@ -4,38 +4,44 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.kenux.miraclelibrary.domain.enums.BookStatus;
 
+import java.time.LocalDate;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class BookTest {
 
     @Test
-    @DisplayName("책은 제목, 저자, isbn 정보를 가지고 생성된다.")
-    void test_Book_Basic() {
+    @DisplayName("책은 제목, 저자, isbn, 등록일자를 가진다.")
+    void test_create_book() {
         Book book = createBook();
 
         assertThat(book.getTitle()).isEqualTo("제목");
         assertThat(book.getAuthor()).isEqualTo("저자");
         assertThat(book.getIsbn()).isEqualTo("isbn");
+        assertThat(book.getCreatedDate()).isEqualTo(createDate());
     }
 
     @Test
     @DisplayName("책은 대여 가능, 대여중, 파기됨 상태를 변경할 수 있다.")
-    void test_Book_changeStatus() throws Exception {
+    void test_change_bookStatus() throws Exception {
         Book book = createBook();
         book.changeStatus(BookStatus.AVAILABLE);
         assertThat(book.getStatus()).isEqualTo(BookStatus.AVAILABLE);
 
-
         book.changeStatus(BookStatus.RENTED);
         assertThat(book.getStatus()).isEqualTo(BookStatus.RENTED);
     }
-
 
     private Book createBook() {
         return Book.builder()
                 .title("제목")
                 .author("저자")
                 .isbn("isbn")
+                .createdDate(createDate())
                 .build();
+    }
+
+    private LocalDate createDate() {
+        return LocalDate.of(2021, 1, 1);
     }
 }
