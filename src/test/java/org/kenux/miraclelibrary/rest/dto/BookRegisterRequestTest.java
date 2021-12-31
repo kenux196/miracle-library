acbd2079726b3328e48c8412747619f7f8ba64ea
kenux196 <kenux.yun@gmail.com>
2021-12-31 12:29:29 +0900

    Modify BookRegisterRequestTest

--- src/test/java/org/kenux/miraclelibrary/rest/dto/BookRegisterRequestTest.java
+++ src/test/java/org/kenux/miraclelibrary/rest/dto/BookRegisterRequestTest.java
@@ -1,5 +1,6 @@
 package org.kenux.miraclelibrary.rest.dto;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.kenux.miraclelibrary.domain.Book;
@@ -9,37 +10,52 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class BookRegisterRequestTest {
 
-    @Test
-    @DisplayName("새책 등록은 엔티티로 변환할 수 있다.")
-    void test_createRegisterBookDto() {
-        BookRegisterRequest dto = BookRegisterRequest.builder()
-                .title("title")
-                .author("author")
-                .isbn("isbn")
-                .build();
+    private BookRegisterRequest bookRegisterRequest;
 
-        assertThat(dto.getTitle()).isNotNull();
-        assertThat(dto.getAuthor()).isNotNull();
-        assertThat(dto.getIsbn()).isNotNull();
+    @BeforeEach
+    void setup() {
+        bookRegisterRequest = createBookRegisterRequest();
     }
 
     @Test
-    @DisplayName("Book Entity 변환가능하다. 이때, 생성 시간이 있어야 하고, Rentable 상태이어야 한다.")
-    void test_toEntity() throws Exception {
+    @DisplayName("Book Entity 로 변환")
+    void test_toEntity() {
+        Book book = bookRegisterRequest.toEntity();
+
+        assertThat(book).isNotNull();
+        assertThat(book.getTitle()).isEqualTo(bookRegisterRequest.getTitle());
+        assertThat(book.getAuthor()).isEqualTo(bookRegisterRequest.getAuthor());
+        assertThat(book.getIsbn()).isEqualTo(bookRegisterRequest.getIsbn());
+    }
+
+    @Test
+    @DisplayName("book entity 로 변환 시, 책의 대여 상태가 초기화된다.")
+    void test_hasBookStatus_toEntity() throws Exception {
         // given
-        BookRegisterRequest dto = BookRegisterRequest.builder()
+        // when
+        Book book = bookRegisterRequest.toEntity();
+
+        // then
+        assertThat(book.getStatus()).isEqualTo(BookStatus.RENTABLE);
+    }
+
+    @Test
+    @DisplayName("book entity 로 변환 시, 책의 등록 날짜를 가진다.")
+    void test_hasBookCreateDate_toEntity() throws Exception {
+        // given
+        // when
+        Book book = bookRegisterRequest.toEntity();
+
+        // then
+        assertThat(book.getCreateDate()).isNotNull();
+    }
+
+    private BookRegisterRequest createBookRegisterRequest() {
+        return BookRegisterRequest.builder()
                 .title("title")
                 .author("author")
                 .isbn("isbn")
                 .build();
-
-        // when
-        final Book book = dto.toEntity();
-
-        // then
-        assertThat(book).isNotNull();
-        assertThat(book.getStatus()).isEqualTo(BookStatus.RENTABLE);
-        assertThat(book.getCreateDate()).isNotNull();
     }
 
 }
