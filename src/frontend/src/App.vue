3f0e9c7388ac6a82ad7c003b6a9d4cd75384cdfb
kenux196 <kenux.yun@gmail.com>
2022-01-12 14:19:06 +0900

    [front] Modify main layout

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,18 +1,26 @@
 <template>
   <div id="app">
-    <b-nav class="w-70 p-3">
-      <b-nav-item to="/">홈</b-nav-item>
-      <b-nav-item-dropdown id="my-nav-dropdown" text="도서" toggle-class="nav-link-custom" right>
-        <b-dropdown-item to="/book">도서</b-dropdown-item>
-        <b-dropdown-item to="/book-manage">책 관리</b-dropdown-item>
-        <b-dropdown-divider></b-dropdown-divider>
-        <b-dropdown-item>Three</b-dropdown-item></b-nav-item-dropdown
-      >
-      <b-nav-item to="/myInfo">내 정보</b-nav-item>
-      <b-nav-item to="/about">About</b-nav-item>
-    </b-nav>
-    <sub-nav></sub-nav>
-    <router-view />
+    <b-container>
+      <b-row>
+        <b-col col md="auto">
+          <b-nav vertical class="w-70 p-3">
+            <b-nav-item to="/">홈</b-nav-item>
+            <b-nav-item-dropdown id="my-nav-dropdown" text="도서" toggle-class="nav-link-custom" right>
+              <b-dropdown-item to="/book">도서</b-dropdown-item>
+              <b-dropdown-item to="/book-manage">책 관리</b-dropdown-item>
+              <b-dropdown-divider></b-dropdown-divider>
+              <b-dropdown-item>Three</b-dropdown-item></b-nav-item-dropdown
+            >
+            <b-nav-item to="/myInfo">내 정보</b-nav-item>
+            <b-nav-item to="/about">About</b-nav-item>
+          </b-nav>
+        </b-col>
+        <b-col>
+          <sub-nav></sub-nav>
+          <router-view />
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
