df7b3405baee36a6ba5304e4d19114d405a7d265
kenux196 <kenux.yun@gmail.com>
2022-01-19 16:47:16 +0900

    Modify build error

--- src/main/java/org/kenux/miraclelibrary/domain/book/repository/BookCustomRepositoryImpl.java
+++ src/main/java/org/kenux/miraclelibrary/domain/book/repository/BookCustomRepositoryImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.time.LocalDate;
 import java.util.List;
 
-import static org.kenux.miraclelibrary.domain.QBook.book;
+import static org.kenux.miraclelibrary.domain.book.domain.QBook.book;
 
 @Repository
 @RequiredArgsConstructor
