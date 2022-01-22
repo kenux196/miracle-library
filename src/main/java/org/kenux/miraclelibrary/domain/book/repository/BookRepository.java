6ab972a21fd09dedfd75f2aa4f38c40fdec2029d
skyun <skyun@a2tec.co.kr>
2022-01-22 13:54:05 +0900

    Refactoring: 테스트 코드 일관성을 위한 규칙 적용

--- src/main/java/org/kenux/miraclelibrary/domain/book/repository/BookRepository.java
+++ src/main/java/org/kenux/miraclelibrary/domain/book/repository/BookRepository.java
@@ -8,7 +8,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long>, BookCustomRepository {
-    int countByStatus(BookStatus rented);
 
     List<Book> findAllByCategory(BookCategory category);
+
+    List<Book> findAllByStatus(BookStatus bookStatus);
 }
