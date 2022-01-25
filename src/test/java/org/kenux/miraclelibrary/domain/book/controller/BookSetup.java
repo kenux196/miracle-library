130b3cd675d259113544c201e82d6b2b5618fefc
skyun <skyun@a2tec.co.kr>
2022-01-25 13:27:40 +0900

    Update from original source

--- src/test/java/org/kenux/miraclelibrary/domain/book/controller/BookSetup.java
+++ src/test/java/org/kenux/miraclelibrary/domain/book/controller/BookSetup.java
@@ -1,6 +1,7 @@
 package org.kenux.miraclelibrary.domain.book.controller;
 
 import org.kenux.miraclelibrary.domain.book.domain.Book;
+import org.kenux.miraclelibrary.domain.book.domain.BookStatus;
 import org.kenux.miraclelibrary.domain.book.dto.BookRegisterRequest;
 import org.kenux.miraclelibrary.domain.book.repository.BookRepository;
 import org.springframework.stereotype.Component;
@@ -15,6 +16,7 @@ public class BookSetup {
 
     public Book saveBook(BookRegisterRequest bookRegisterRequest) {
         final Book book = bookRegisterRequest.toEntity();
+        book.changeStatus(BookStatus.RENTABLE);
         return bookRepository.save(book);
     }
 }
