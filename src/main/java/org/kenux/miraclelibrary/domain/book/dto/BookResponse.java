8f045879ad4f004989b1ae2354efcbbe79251632
kenux196 <kenux.yun@gmail.com>
2022-01-26 19:23:27 +0900

    Modify BookResponse

--- src/main/java/org/kenux/miraclelibrary/domain/book/dto/BookResponse.java
+++ src/main/java/org/kenux/miraclelibrary/domain/book/dto/BookResponse.java
@@ -4,9 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import org.kenux.miraclelibrary.domain.book.domain.Book;
+import org.kenux.miraclelibrary.domain.book.domain.BookCategory;
 import org.kenux.miraclelibrary.domain.book.domain.BookStatus;
-
-import java.time.LocalDate;
 
 @Data
 @Builder
@@ -15,18 +14,18 @@ public class BookResponse {
     private Long bookId;
     private String title;
     private String author;
-    private String isbn;
-    private LocalDate publicationDate;
+    private BookCategory category;
     private BookStatus status;
+    private String cover;
 
     public static BookResponse from(Book book) {
         return BookResponse.builder()
                 .bookId(book.getId())
                 .title(book.getTitle())
                 .author(book.getAuthor())
-                .isbn(book.getIsbn())
-                .publicationDate(book.getPublicationDate())
                 .status(book.getStatus())
+                .cover(book.getCover())
+                .category(book.getCategory())
                 .build();
     }
 }
