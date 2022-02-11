7bfd17479808dec831fe08e437f48a5046677546
kenux196 <kenux.yun@gmail.com>
2022-02-11 15:52:28 +0900

    [화면] Thymeleaf + bootstrap

--- src/main/java/org/kenux/miraclelibrary/domain/boardtest/web/controller/BoardTestController.java
+++ src/main/java/org/kenux/miraclelibrary/domain/boardtest/web/controller/BoardTestController.java
@@ -14,7 +14,14 @@ import java.util.List;
 public class BoardTestController {
 
     @GetMapping
-    public String indexPage() {
+    public String indexPage(Model model) {
+        List<Board> boards = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            int number = i + 1;
+            Board board = new Board(number, "테스트 중..." + number , "윤상규");
+            boards.add(board);
+        }
+        model.addAttribute("boardList", boards);
         return "index";
     }
 
